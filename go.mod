073fda8357dbdb5fc264b4a9d3e87dd22cd93e3b
Gabriel Ochsenhofer <gabs@gabs.tv>
2019-02-24 03:15:35 -0300

    working on bdiff funcs

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/gabstv/go-bsdiff
+
+require github.com/dsnet/compress v0.0.0-20171208185109-cc9eb1d7ad76
--- pkg/bsdiff/bsdiff.go
+++ pkg/bsdiff/bsdiff.go
@@ -1,8 +1,11 @@
 package bsdiff
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+
+	"github.com/dsnet/compress/bzip2"
 )
 
 const (
@@ -39,16 +42,100 @@ func DiffFiles(oldbin io.ReadSeeker, newbin io.ReadSeeker, diffbin io.Writer) er
 }
 
 func diffb(oldbin, newbin []byte) ([]byte, error) {
-	iii := make([]int32, len(oldbin)+1)
-	vvv := make([]int32, len(oldbin)+1)
+	iii := make([]int, len(oldbin)+1)
+	vvv := make([]int, len(oldbin)+1)
 	qsufsort(iii, vvv, oldbin)
 	// [C] free(V)
 	vvv = nil
+
+	//var db
+	var dblen, eblen int
+
+	// create the patch file
+	pf := new(bytes.Buffer)
+
+	// Header is
+	//	0	8	 "BSDIFF40"
+	//	8	8	length of bzip2ed ctrl block
+	//	16	8	length of bzip2ed diff block
+	//	24	8	length of pnew file */
+	//	/* File is
+	//		0	32	Header
+	//		32	??	Bzip2ed ctrl block
+	//		??	??	Bzip2ed diff block
+	//		??	??	Bzip2ed extra block
+
+	header := make([]byte, 32)
+
+	copy(header, []byte("BSDIFF40"))
+	offtout(0, header[8:])
+	offtout(0, header[16:])
+	offtout(len(newbin), header[24:])
+	if _, err := pf.Write(header); err != nil {
+		return nil, err
+	}
+	// Compute the differences, writing ctrl as we go
+	pfbz2, err := bzip2.NewWriter(pf, nil)
+	if err != nil {
+		return nil, err
+	}
+	var scan, ln, lastscan, lastpos, lastoffset int
+	newsize := len(newbin)
+	oldsize := len(oldbin)
+
+	var oldscore, scsc int
+
+	for scan < newsize {
+		oldscore = 0
+
+		scan += ln
+		scsc = scan
+		for scan < newsize {
+			scan++
+			//ln = search(iii, oldbin, oldsize, newbin[scan:], newsize - scan, 0, oldsize, &pos)
+		}
+	}
+
 	return nil, fmt.Errorf("not implemented")
 }
 
-func qsufsort(iii, vvv []int32, buf []byte) {
-	buckets := make([]int32, 256)
+func offtout(x int, buf []byte) {
+	var y int
+	if x < 0 {
+		y = -x
+	} else {
+		y = x
+	}
+	buf[0] = byte(y % 256)
+	y -= int(buf[0])
+	y = y / 256
+	buf[1] = byte(y % 256)
+	y -= int(buf[1])
+	y = y / 256
+	buf[2] = byte(y % 256)
+	y -= int(buf[2])
+	y = y / 256
+	buf[3] = byte(y % 256)
+	y -= int(buf[3])
+	y = y / 256
+	buf[4] = byte(y % 256)
+	y -= int(buf[4])
+	y = y / 256
+	buf[5] = byte(y % 256)
+	y -= int(buf[5])
+	y = y / 256
+	buf[6] = byte(y % 256)
+	y -= int(buf[6])
+	y = y / 256
+	buf[7] = byte(y % 256)
+
+	if x < 0 {
+		buf[7] |= 0x80
+	}
+}
+
+func qsufsort(iii, vvv []int, buf []byte) {
+	buckets := make([]int, 256)
 	var i, h, ln int
 	bufzise := len(buf)
 	// [C] for (i = 0;i < 256;i++) buckets[i] = 0;
@@ -69,12 +156,12 @@ func qsufsort(iii, vvv []int32, buf []byte) {
 	// [C] for (i = 0;i < oldsize;i++) I[++buckets[pold[i]]] = i;
 	for i = 0; i < bufzise; i++ {
 		buckets[buf[i]]++
-		iii[buckets[buf[i]]] = int32(i)
+		iii[buckets[buf[i]]] = i
 	}
-	iii[0] = int32(bufzise)
+	iii[0] = bufzise
 	// [C] for (i = 0;i < oldsize;i++) V[i] = buckets[pold[i]];
 	for i = 0; i < bufzise; i++ {
-		vvv[i] = int32(buckets[buf[i]])
+		vvv[i] = buckets[buf[i]]
 	}
 	vvv[bufzise] = 0
 	// [C] for (i = 1;i < 256;i++) if (buckets[i] == buckets[i - 1] + 1) I[buckets[i]] = -1;
@@ -85,41 +172,111 @@ func qsufsort(iii, vvv []int32, buf []byte) {
 	}
 	iii[0] = -1
 	// [C] for (h = 1;I[0] != -(oldsize + 1);h += h) {
-	for h = 1; iii[0] != int32(-(bufzise + 1)); h += h {
+	for h = 1; iii[0] != (-(bufzise + 1)); h += h {
 		ln = 0
 		// [C] for (i = 0;i < oldsize + 1;) {
 		i = 0
 		for i < bufzise+1 {
 			if iii[i] < 0 {
-				ln -= int(iii[i])
-				i -= int(iii[i])
+				ln -= (iii[i])
+				i -= (iii[i])
 			} else {
 				if ln != 0 {
-					iii[i-ln] = int32(-ln)
+					iii[i-ln] = (-ln)
 				}
-				ln = int(vvv[iii[i]] + 1 - int32(i))
+				ln = (vvv[iii[i]] + 1 - (i))
 				split(iii, vvv, i, ln, h)
 				i += ln
 				ln = 0
 			}
 		}
 		if ln != 0 {
-			iii[i-ln] = int32(-ln)
+			iii[i-ln] = (-ln)
 		}
 	}
 
 	for i = 0; i < bufzise+1; i++ {
-		iii[vvv[i]] = int32(i)
+		iii[vvv[i]] = (i)
 	}
 }
 
-func split(iii, vvv []int32, start, ln, h int) {
-	var i, j, k, x, tmp, jj, kk int
+func split(iii, vvv []int, start, ln, h int) {
+	var i, j, k, x int
+	//var tmp int32
 
 	if ln < 16 {
 		for k = start; k < start+ln; k += j {
-
+			j = 1
+			x = (vvv[(iii[k] + (h))])
+			for i = 1; k+i < start+ln; i++ {
+				if vvv[(iii[k+i]+(h))] < (x) {
+					x = int(vvv[int(iii[k+i]+(h))])
+					j = 0
+				}
+				if vvv[int(iii[k+i]+(h))] == x {
+					//tmp = iii[k+j]
+					//iii[k+j]=iii[k+i]
+					//iii[k+i]=tmp
+					iii[k+j], iii[k+i] = iii[k+i], iii[k+j]
+					j++
+				}
+				for i = 0; i < j; i++ {
+					vvv[iii[k+i]] = k + j - 1
+				}
+				if j == 1 {
+					iii[k] = -1
+				}
+			}
 		}
+		return
+	}
+
+	x = vvv[iii[start+ln/2]+h]
+	var jj, kk int
+	for i = start; i < start+ln; i++ {
+		if vvv[iii[i]+h] < x {
+			jj++
+		} else if vvv[iii[i]+h] == x {
+			kk++
+		}
+	}
+	jj += start
+	kk += jj
+
+	i = start
+	k = 0
+	for i < jj {
+		if vvv[iii[i]+h] < x {
+			i++
+		} else if vvv[iii[i]+h] == x {
+			iii[i], iii[jj+j] = iii[jj+j], iii[i]
+			j++
+		} else {
+			iii[i], iii[kk+k] = iii[kk+k], iii[i]
+			k++
+		}
+	}
+	for jj+j < kk {
+		if vvv[iii[jj+j]+h] == x {
+			j++
+		} else {
+			iii[jj+j], iii[kk+k] = iii[kk+k], iii[jj+j]
+			k++
+		}
+	}
+	if jj > start {
+		split(iii, vvv, start, jj-start, h)
+	}
+
+	for i = 0; i < kk-jj; i++ {
+		vvv[iii[jj+i]] = kk - 1
+	}
+	if jj == kk-1 {
+		iii[jj] = -1
+	}
+
+	if start+ln > kk {
+		split(iii, vvv, kk, start+ln-kk, h)
 	}
 }
 
